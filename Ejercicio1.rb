c7e04c4b3875d489d5b616257702337bcfb20371
TheDevilM <162632038+TheDevilM@users.noreply.github.com>
2024-08-16 19:59:36 -0600

    Add files via upload

---
+++ Ejercicio1.rb
@@ -0,0 +1,8 @@
+#array 
+semana=["Lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+puts semana
+puts "\nImprimir por posicion"
+puts semana[1]
+puts semana[3]
+puts semana[5]
---
+++ Ejercicio3.rb
@@ -0,0 +1,25 @@
+semana=["Lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+puts "Array en ruby"
+puts semana
+
+puts "\nMetodo to_s"
+puts semana.to_s
+
+puts "\nMetodo pop"
+puts semana.pop
+
+puts "\nMetodo join"
+puts semana.join(",")
+
+puts "\nMetodo first"
+puts semana.first
+
+puts "\nMetodo last"
+puts semana.last
+
+puts "\nMetodo push"
+puts semana.push "Final"
+
+puts "\nMetodo length"
+puts semana.length
